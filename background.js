cafab9d0ece78b15ec1f205d047056b486dc6de8
raccoon254 <tomsteve187@gmail.com>
2024-07-22 19:56:44 +0300

    Complete UI fix styles

--- background.js
+++ background.js
@@ -1,7 +1,18 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'next' || message.action === 'prev' || message.action === 'toggleAuto') {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, message);
+      if (tabs.length > 0) {
+        chrome.tabs.sendMessage(tabs[0].id, message, (response) => {
+          if (chrome.runtime.lastError) {
+            console.error('Error sending message to content script:', chrome.runtime.lastError);
+          } else {
+            sendResponse(response);
+          }
+        });
+      } else {
+        console.error('No active tabs found.');
+      }
     });
+    return true; // Indicate that the response will be sent asynchronously
   }
 });
